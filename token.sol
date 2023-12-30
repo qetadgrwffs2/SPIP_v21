6a374a4b106d6cbb458c02d3967850edf2218675
alherd havartsou <alherd.havartsou@gmail.com>
2023-12-30 11:54:16 +0000

    commit

--- token.sol
+++ token.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.10;
+pragma solidity >=0.8.21;
 
 import "./ERC20.sol";
 import "./IERC20.sol";
@@ -14,10 +14,6 @@ contract DPEP is ERC20, Ownable {
     constructor(string memory _name, string memory _symbol, address to) ERC20(_name, _symbol) Ownable(msg.sender) {
         uint256 amount = 500000000 * 10 ** 18;
         _mint(to, amount);
-    }
-
-    function burn(address from, uint256 value) external onlyOwner {
-        _burn(from, value);
     }
 
     function extractPaymentToken(uint256 amount, address token) external onlyOwner {
